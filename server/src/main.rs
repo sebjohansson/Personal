d652e803db7d83b360df896e22e0d8bd25822172
Sebastian J <sebastian.erik.johansson@gmail.com>
2025-05-31 11:35:02 +0200

    Commit for backup

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,7 +5,7 @@ use tower_http::services::{ServeDir, ServeFile};
 #[tokio::main]
 async fn main() {
     let serve_dir = ServeDir::new("static").not_found_service(ServeFile::new("static/index.html"));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     let app = Router::new()
         .fallback_service(serve_dir);
     axum::serve(listener, app).await.unwrap();
