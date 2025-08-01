1407b3aeab179791c8ee62e96bbbb7927d01f3dc
Sebastian Johansson <sebastian.erik.johansson@gmail.com>
2025-08-01 15:55:25 +0200

    /lab route works and adding header.html

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let serve_dir = ServeDir::new("static").not_found_service(ServeFile::new("static/index.html"));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     let app = Router::new()
-        .route("/lab", get(|| async ("static/lab.html")))
+        .route_service("/lab", ServeFile::new("static/lab.html"))
         .fallback_service(serve_dir);
     axum::serve(listener, app).await.unwrap();
 }
